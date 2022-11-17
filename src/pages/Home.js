b985d9026494c5a2cc4488a66f19e7d92aa9020b
Cécile <alfonso.cilou@gmail.com>
2022-11-17 15:46:33 +0100

    composition de la page accueil

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Banner from '../components/Banner';
+import Gallery from '../components/Gallery';
+import Header from '../components/Header';
 
 const Home = () => {
     return (
         <div>
-            <h1>accueil</h1>
+            <Header />
+            <div className='banner'>
+                <Banner />
+            </div>
+            <div>
+                <Gallery />
+            </div>
         </div>
     );
 };
