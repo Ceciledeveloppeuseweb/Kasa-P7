1a74d7504a7a933516c7313d0a012bf81747e147
Cécile <alfonso.cilou@gmail.com>
2022-11-17 15:47:12 +0100

    composition page 404

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Header from '../components/Header';
+
 
 const Error = () => {
     return (
+        
         <div>
+            <Header />
             <h1>Oups ! La page que vous demandez n'existe pas.</h1>
         </div>
     );
