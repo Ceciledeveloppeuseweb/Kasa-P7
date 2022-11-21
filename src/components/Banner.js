de38629f2903b794b3ed0821a2661d7cda1f87bf
Cécile <alfonso.cilou@gmail.com>
2022-11-21 21:22:05 +0100

    revue composant banniere re-utilisable

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,14 +1,7 @@
-import React from 'react';
-import banner from '../assets/banner.jpg';
+import React from "react";
 
-const Banner = () => {
-    return (
-        <div className='fond-banner'>
-            <h1 className='titre-banner'>Chez vous, partout et ailleurs</h1>
-            <img src={banner} alt="bannière paysage" className='img-banner'/>
-        </div>
-    );
+const Banner = ({children}) => {
+  return <div className="banner">{children}</div>;
 };
-            
 
 export default Banner;
