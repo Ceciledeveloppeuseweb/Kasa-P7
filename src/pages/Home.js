84f848075a64f7cb6a590d206c02f44e9fbca681
Cécile <alfonso.cilou@gmail.com>
2022-11-21 21:22:59 +0100

    création props sur Banner Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import Banner from '../components/Banner';
-import Gallery from '../components/Gallery';
-import Header from '../components/Header';
-
+import React from "react";
+import Banner from "../components/Banner";
+import Gallery from "../components/Gallery";
+import Header from "../components/Header";
+import banner from "../assets/banner.jpg";
 
 const Home = () => {
-    return (
-        <div>
-            <Header />
-            <div className='banner'>
-                <Banner />
-            </div>
-            <Gallery />
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+
+      <Banner>
+        <h1 className="titre-banner">Chez vous, partout et ailleurs</h1>
+        <img src={banner} alt="bannière paysage" className="img-banner" />
+      </Banner>
+
+      <Gallery />
+    </div>
+  );
 };
 
 export default Home;
