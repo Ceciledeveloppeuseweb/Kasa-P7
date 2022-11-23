825cc96bb7efda15dd2f2e0bb80efcf087e8f6d8
Cécile <alfonso.cilou@gmail.com>
2022-11-23 13:31:26 +0100

    mise en place du css des collapses et modif code

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,16 +1,25 @@
 import Button from "./Button";
 import React, { useState } from "react";
 
-const Collapse = ({title, description}) => {
-  const [toggle, setToggle] = useState(false);
+const Collapse = ({ title, description }) => {
+  const [isOpen, setIsOpen] = useState(false);
 
-  return (
+  return isOpen ? (
     <div className="collapse-container">
-      <div className="collapse">
-        <h2 className="button-toggle">{title}</h2>
-        <Button onClick={() => setToggle(!toggle)} />
+      <div className="collapse" onClick={() => setIsOpen(false)}>
+        <h2 className="collapse-titre">{title}</h2>
+        <Button />
       </div>
-      {toggle && <p>{description}</p>}
+      <div className="description">
+        <p>{description}</p>
+      </div>
+    </div>
+  ) : (
+    <div className="collapse-container">
+      <div className="collapse" onClick={() => setIsOpen(true)}>
+        <h2 className="button-toggle">{title}</h2>
+        <Button />
+      </div>
     </div>
   );
 };
