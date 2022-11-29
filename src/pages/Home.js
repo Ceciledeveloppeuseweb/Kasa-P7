2f9d67915a970b5c813b5a2ed223e62b02dc86b6
Cécile <alfonso.cilou@gmail.com>
2022-11-29 14:11:28 +0100

    page home ok

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,45 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
 import banner from "../assets/banner-home.jpg";
 import Footer from "../components/Footer";
-import locations from "../datas/locations.json";
 import { NavLink } from "react-router-dom";
+//import Loader from "../components/Loader";
 
 const Home = () => {
+  const [data, setData] = useState([]);
+  const getData = () => {
+    fetch("/locations.JSON")
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((error) => console.log(error));
+  };
+
+  //avec la méthode "axios" qui ne nécessite pas de convertir en json
+  //const getData = () => {   //=> il faut préciser la méthode de la requête
+  //  axios.get("../../public/locations")
+  //.then((res) => setData(res.data))  //=>res.data pour ne récupérer que les datas et non toute la réponse avec les headers et autres!
+  //.catch((err) => console.log(err))
+  //}
+
+  useEffect(() => {
+    getData();
+  }, []);
+//console.log(getData);
   return (
     <>
       <Header />
 
       <Banner className="banner">
         <h1 className="titre-banner">Chez vous, partout et ailleurs</h1>
-        <img src={banner} alt="paysage côte rocheuse" className="img-banner " />
+        <img src={banner} alt="paysage côte rocheuse" className="img-banner" />
       </Banner>
 
-      <main className="cards">
-        {locations.map((location) => {
-          return (
-            <NavLink
-              to={"/location/" + location.id}
-              className="linkCard"
-              key={location.id}
-            >
-              <article className="location">
-                <img
-                  src={location.cover}
-                  alt={location.title}
-                  className="img-location"
-                />
-                <h2 className="titre-location">{location.title}</h2>
-              </article>
-            </NavLink>
-          );
-        })}
-      </main>
-
+      
+        <main className="cards">
+          {data.map((location) => (
+           // console.log(location);
+              <NavLink to={"/location/" + location.id} className="linkCard" key={location.id}>
+                <article className="location">
+                  <img
+                    src={location.cover}
+                    alt={location.title}
+                    className="img-location"
+                  />
+                  <h2 className="titre-location">{location.title}</h2>
+                </article>
+              </NavLink>
+            )
+          )}
+        </main>
       <Footer />
     </>
   );
 };
 
 export default Home;
+            
+
+            
+      
